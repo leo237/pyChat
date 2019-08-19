2811771ea493bb5402ddb1405a9c8caf4f068149
Abhilash Panigrahi <pabhila@amazon.com>
2019-08-18 17:51:57 -0700

    minor improvements

--- client.py
+++ client.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python           # This is client.py file
 
 import socket               # Import socket module
+from termcolor import colored
+
+host = "192.168.1.6"
+#host = socket.gethostname() # Get local machine name
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-#host = "192.168.1.38"
+print("Host : {}".format(host))
 port = 12345   
+
+name = raw_input("Enter your name :")
 
 s.connect((host, port))
 while True:
-	msg = raw_input("Message:")
-	s.send(msg)
-	print "server:", s.recv(1024)
+        msg = raw_input("> ")
+        s.send("{} : {}".format(name, msg))
+        print(colored(s.recv(1024), 'green'))
 s.close
 
 
--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python           # This is server.py file
 
 import socket               # Import socket module
-lol = ''
+from termcolor import colored
+
+#host = socket.gethostname() # Get local machine name
+host = '192.168.1.6'
+
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+print("Host : {}".format(host))
 port = 12345              # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
+name = raw_input("Enter your name:")
+
 s.listen(5) 
 c, addr = s.accept()                   # Now wait for client connection.
-while lol != 'exit':
-   lol = c.recv(1024)  
-   print addr , ": " , lol
-   msg = raw_input("Message:")
-   c.send(msg)
+while True:
+   rcv_message = c.recv(1024)  
+   print(colored(rcv_message, 'green'))
+   msg = raw_input("> ")
+   c.send("{} : {}".format(name, msg))
 c.close() 
